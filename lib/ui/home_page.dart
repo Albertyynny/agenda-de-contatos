fa61450bb0073bdc8cc5c218e28f71b3fe45d3b7
Samuel Mataraso <samuelmataraso@gmail.com>
2019-07-01 17:37:51 -0300

    feat/ starting implementing the home_page.

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -9,26 +9,37 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   ContactHelper helper = ContactHelper();
 
+  List<Contact> contacts = List();
+
   @override
   void initState() {
     super.initState();
-
-    // Teste insert data on database
-    // Contact c = Contact();
-    // c.name = "Samuca";
-    // c.email = "samuel@gmail.com";
-    // c.phone = "sauhsuhasu";
-    // c.img = "imgT";
-    // helper.saveContact(c);
-
-    // Teste get data of database
     helper.getAllContacts().then((list) {
-      print(list);
+      setState(() {
+        contacts = list;
+      });
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Contatos"),
+        backgroundColor: Colors.red,
+        centerTitle: true,
+      ),
+      backgroundColor: Colors.white,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
+        backgroundColor: Colors.red,
+      ),
+      body: ListView.builder(
+        padding: EdgeInsets.all(10.0),
+        itemCount: contacts.length,
+        itemBuilder: (context, index) {},
+      ),
+    );
   }
 }

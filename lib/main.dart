ba4eebc72a487cd7af9259c9ccfb1cc5a32d971e
Samuel Mataraso <samuelmataraso@gmail.com>
2019-07-01 18:58:03 -0300

    feat/ send contacts between screens.

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
 
 import 'ui/home_page.dart';
-import 'ui/contact_page.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: ContactPage(),
+    home: HomePage(),
     debugShowCheckedModeBanner: false,
   ));
 }
